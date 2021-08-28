5bd290ad6c73d0a212ed690d6b4c23e217b2d11f
JCCPGit <cortezperezjc@gmail.com>
2021-08-28 13:03:48 -0700

    Final Version

--- Process/app_final.py
+++ Process/app_final.py
@@ -39,52 +39,39 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start</br>"
-        f"/api/v1.0/start-end</br>"
+        f"/api/v1.0/start/end"
     )
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     
-    precipitation = session.query(Measurement.date, Measurement.prcp).\
+    results = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= year_ago).all()
     
-    precip = {date: prcp for date, prcp in precipitation}
-    return jsonify(precip)
+    rows = {date: prcp for date, prcp in precipitation}
+    return jsonify(rows)
 
 @app.route("/api/v1.0/stations")
 def stations():
+    results = session.query(Station.station).all()
     
-    results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
-    
-    all_stations = []
-    for station, name, latitude, longitude, elevation in results:
-        all_stations_dict = {}
-        all_stations_dict["Stations"] = station
-        all_stations_dict["Name"] = name
-        all_stations_dict["Latitude"] = latitude
-        all_stations_dict["Longitude"] = longitude
-        all_stations_dict["Elevation"] = elevation
-        all_stations.append(all_stations_dict)
-
-    return jsonify(all_stations)
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
 
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-    most_active_station = 'USC00519281'
     
-    results = session.query(Measurement.date, Measurement.tobs).\
-        filter(Measurement.station == most_active_station).\
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
         filter(Measurement.date >= year_ago).all()
     
-    rows = [{"Date":r[0], "Temperature":r[1]} for r in results]
-    
-    return jsonify(rows)
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
 
-@app.route("/api/v1.0/temp/start")
-@app.route("/api/v1.0/start-end")
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/<start>/<end>")
 def stats(start=None, end=None):
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
     
